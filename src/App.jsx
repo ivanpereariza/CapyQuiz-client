6997359d698efaf3bda44dbf05651c439ef95e89
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-03-04 11:09:31 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
-      <AppRoutes />
+      <>
+        <Navbar />
+        <AppRoutes />
+      </>
     </div>
   );
 }
