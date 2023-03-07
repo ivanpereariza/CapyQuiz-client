0586091b848dd27c0621747e29284e28225bcb37
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-03-07 19:44:11 +0100

    d

--- src/components/QuizDetails/QuizDetails.jsx
+++ src/components/QuizDetails/QuizDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import SpinnerLoader from '../SpinnerLoader/SpinnerLoader'
 import './../QuizDetails/QuizDetails.css'
@@ -16,35 +16,40 @@ const QuizDetails = ({ quiz }) => {
 
     return (
         <Container className='d-flex justify-content-center'>
-            {
-                quiz ?
-                    <div className='justify-content-center'>
-                        <img className='QuizDetailsImg' src={quiz.quizImg} alt="quiz Image" />
-                        <h3 className='mb-4'> {quiz.title}</h3>
-                        <p><b>Theme:</b> {quiz.theme}</p>
-                        <p><b>Description:</b> {quiz.description}</p>
-                        <p><b>Questions:</b> {quiz.questionsArr.length}</p>
-                        <p><b>Estimated Time:</b> {time}</p>
-                        {
-                            quiz.owner ?
-                                <div className='d-flex justify-content-start'>
-                                    <p className='pt-2'><b>Author:</b> {quiz.owner.username}</p>
-                                    <Link to={`/profile/${quiz.owner._id}`} className='d-grid mb-5' >
-                                        <img className='ownerAvatar' src={`${quiz.owner.avatar}`} alt={quiz.owner.username} />
-                                    </Link>
-                                </div>
-                                :
-                                undefined
-                        }
-                        <Link to={`/quizzes/play/${quiz._id}`} className='d-grid mb-5' >
-                            <Button type="submit" variant={`outline-${theme} mt-4`}>Start Game!</Button>
-                        </Link>
+            <Row>
+                <Col sm={{ span: 8, offset: 1 }} md={{ span: 8, offset: 2 }}>
 
-                    </div>
-                    :
-                    <SpinnerLoader />
-            }
+                    {
+                        quiz ?
+                            <div className='justify-content-center'>
+                                <img className='QuizDetailsImg' src={quiz.quizImg} alt="quiz Image" />
+                                <h3 className='mb-4'> {quiz.title}</h3>
+                                <p><b>Theme:</b> {quiz.theme}</p>
+                                <p><b>Description:</b> {quiz.description}</p>
+                                <p><b>Questions:</b> {quiz.questionsArr.length}</p>
+                                <p><b>Estimated Time:</b> {time}</p>
+                                {
+                                    quiz.owner ?
+                                        <div className='d-flex justify-content-start'>
+                                            <p className='pt-2'><b>Author:</b> {quiz.owner.username}</p>
+                                            <Link to={`/profile/${quiz.owner._id}`} className='d-grid mb-5' >
+                                                <img className='ownerAvatar' src={`${quiz.owner.avatar}`} alt={quiz.owner.username} />
+                                            </Link>
+                                        </div>
+                                        :
+                                        undefined
+                                }
+                                <Link to={`/quizzes/play/${quiz._id}`} className='d-grid mb-5' >
+                                    <Button type="submit" variant={`outline-${theme} mt-4`}>Start Game!</Button>
+                                </Link>
 
+                            </div>
+                            :
+                            <SpinnerLoader />
+                    }
+
+                </Col>
+            </Row>
         </Container>
     )
 }
---
+++ src/components/QuizOwnerCard/QuizOwnerCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { useContext } from "react"
+import { ThemeContext } from "../../contexts/theme.context"
+import getEstimatedTime from '../../utils/getEstimatedTime'
+import { Link } from 'react-router-dom'
+
+const QuizOwnerCard = ({ quiz }) => {
+
+    const { themeValue } = useContext(ThemeContext)
+    const themeColor = themeValue === 'light' ? 'dark' : 'light'
+    const { title, theme, description, questionsArr, quizImg, _id } = quiz
+
+    const time = getEstimatedTime(questionsArr)
+
+    return (
+        <Card className={`${themeValue} card my-3`} style={{ height: '35rem' }}>
+            <Card.Body>
+                <Card.Img className='mb-3' variant='top' src={quizImg} alt="Quiz Img" />
+                <Card.Title className='mb-4'> {title}</Card.Title>
+                <Card.Text><b>Theme:</b> {theme}</Card.Text>
+                <Card.Text><b>Description:</b> {description}</Card.Text>
+                <Card.Text><b>Estimated Time:</b> {time}</Card.Text>
+                <hr />
+                <Link to={`/quizzes/play/${quiz._id}`} className='d-grid mb-5' >
+                    <Button type="submit" variant={`outline-${themeColor} mt-4`}>Start Game!</Button>
+                </Link>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default QuizOwnerCard
