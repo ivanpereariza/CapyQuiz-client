2cad70399858680ab2db5b025fd8ab827c9ca7af
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-03-08 12:29:07 +0100

    d

--- src/components/QuizDetails/QuizDetails.jsx
+++ src/components/QuizDetails/QuizDetails.jsx
@@ -16,41 +16,36 @@ const QuizDetails = ({ quiz }) => {
 
     return (
         <Container className='d-flex justify-content-center'>
-            <Row>
-                <Col sm={{ span: 8, offset: 1 }} md={{ span: 8, offset: 2 }}>
+            {
+                quiz ?
+                    <div className='justify-content-center'>
+                        <img className='QuizDetailsImg d-block mx-auto' src={quiz.quizImg} alt="quiz Image" />
+                        <h3 className='mb-4'> {quiz.title}</h3>
+                        <p><b>Theme:</b> {quiz.theme}</p>
+                        <p><b>Description:</b> {quiz.description}</p>
+                        <p><b>Questions:</b> {quiz.questionsArr.length}</p>
+                        <p><b>Estimated Time:</b> {time}</p>
+                        {
+                            quiz.owner ?
+                                <div className='d-flex justify-content-start'>
+                                    <p className='pt-2'><b>Author:</b> {quiz.owner.username}</p>
+                                    <Link to={`/profile/${quiz.owner._id}`} className='d-grid mb-5' >
+                                        <img className='ownerAvatar' src={`${quiz.owner.avatar}`} alt={quiz.owner.username} />
+                                    </Link>
+                                </div>
+                                :
+                                undefined
+                        }
+                        <Link to={`/quizzes/play/${quiz._id}`} className='d-grid mb-5' >
+                            <Button type="submit" variant={`outline-${theme} mt-4`}>Start Game!</Button>
+                        </Link>
 
-                    {
-                        quiz ?
-                            <div className='justify-content-center'>
-                                <img className='QuizDetailsImg' src={quiz.quizImg} alt="quiz Image" />
-                                <h3 className='mb-4'> {quiz.title}</h3>
-                                <p><b>Theme:</b> {quiz.theme}</p>
-                                <p><b>Description:</b> {quiz.description}</p>
-                                <p><b>Questions:</b> {quiz.questionsArr.length}</p>
-                                <p><b>Estimated Time:</b> {time}</p>
-                                {
-                                    quiz.owner ?
-                                        <div className='d-flex justify-content-start'>
-                                            <p className='pt-2'><b>Author:</b> {quiz.owner.username}</p>
-                                            <Link to={`/profile/${quiz.owner._id}`} className='d-grid mb-5' >
-                                                <img className='ownerAvatar' src={`${quiz.owner.avatar}`} alt={quiz.owner.username} />
-                                            </Link>
-                                        </div>
-                                        :
-                                        undefined
-                                }
-                                <Link to={`/quizzes/play/${quiz._id}`} className='d-grid mb-5' >
-                                    <Button type="submit" variant={`outline-${theme} mt-4`}>Start Game!</Button>
-                                </Link>
+                    </div>
+                    :
+                    <SpinnerLoader />
+            }
 
-                            </div>
-                            :
-                            <SpinnerLoader />
-                    }
-
-                </Col>
-            </Row>
-        </Container>
+        </Container >
     )
 }
 
