9d3f433e4dcd074aaea5865fcf0fcdb5843f9eeb
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-03-11 21:43:18 +0100

    d

--- src/pages/QuizListPage/QuizListPage.jsx
+++ src/pages/QuizListPage/QuizListPage.jsx
@@ -1,3 +1,4 @@
+import { Slider } from '@mui/material'
 import React, { useEffect, useState, useContext } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import NewQuizModal from '../../components/NewQuizModal/NewQuizModal'
@@ -7,6 +8,7 @@ import QuizList from '../../components/QuizList/QuizList'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import { AuthContext } from '../../contexts/auth.context'
 import quizzesService from '../../services/quizzes.services'
+import getAverageRating from '../../utils/getAverageRating'
 
 
 
@@ -18,6 +20,7 @@ const QuizListPage = () => {
     const [showModal, setShowModal] = useState(false)
     const [showModalDetails, setShowModalDetails] = useState(false)
     const [selectedQuiz, setSelectedQuiz] = useState('')
+    const [rankingValue, setRankingValue] = useState([0, 100])
 
     const { user } = useContext(AuthContext)
 
@@ -43,6 +46,12 @@ const QuizListPage = () => {
         setQuizzes(filteredQuizzes)
     }
 
+    const handleRatingBar = (e, newValue) => {
+        setRankingValue(newValue)
+        const filteredQuizzes = quizzesBackUp.filter(elm => getAverageRating(elm) >= e.target.value[0] && getAverageRating(elm) <= e.target.value[1])
+        setQuizzes(filteredQuizzes)
+    }
+
     const fireFinalActions = () => {
         setShowModal(false)
         loadQuizzes()
@@ -58,13 +67,27 @@ const QuizListPage = () => {
             <Container className='mt-4'>
                 <h1>Check the quizzes created by the community!</h1>
                 <hr />
-                <Row>
+                <Row className='align-items-center'>
                     <Col md={{ span: 4 }}>
                         <SearchBar handleSearchBar={handleSearchBar} />
                     </Col>
+                    <Col md={{ span: 3, offset: 1 }}>
+                        <div>
+                            <h4>Filter by rating:</h4>
+                            <Slider
+                                getAriaLabel={() => 'Ranking range'}
+                                value={rankingValue}
+                                onChange={handleRatingBar}
+                                valueLabelDisplay="auto"
+                                max={5}
+                                min={0}
+                                color={'primary'}
+                            />
+                        </div>
+                    </Col>
                     {
                         user ?
-                            <Col className='d-flex' md={{ span: 4 }} >
+                            <Col className='d-flex' md={{ span: 2, offset: 2 }} >
                                 <NewQuizzButton setShowModal={setShowModal} />
                             </Col>
                             :
