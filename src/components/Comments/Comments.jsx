ba72af269316d9aea3bc52bb5a3729906f210cee
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-03-15 12:42:12 +0100

    d

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,5 +1,4 @@
 import { useContext, useEffect, useState } from "react"
-import { OverlayTrigger, Tooltip } from "react-bootstrap"
 import { Button, Card, Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap"
 import { AuthContext } from "../../contexts/auth.context"
 import { ThemeContext } from "../../contexts/theme.context"
@@ -47,53 +46,61 @@ const Comments = ({ quizId }) => {
                 editComment ?
                     <EditCommentForm getComments={getComments} setEditComment={setEditComment} {...messageData} />
                     :
-                    comments?.map((elm, idx) => {
-                        return (
-                            <Card className={`${themeValue} mt-3`} key={idx}>
-                                <div className="p-3">
-                                    <Row className="align-items-center">
-                                        <Col className="me-4 my-1" lg={{ span: 1 }}>
-                                            <OverlayTrigger
-                                                placement="bottom"
-                                                overlay={<Tooltip id={`tooltip-bottom`}>
-                                                    {elm.owner.username}
-                                                </Tooltip>}
-                                            >
-                                                <img className={`${themeValue} ownerAvatar`} src={elm.owner.avatar} alt="" />
-                                            </OverlayTrigger>
-                                        </Col>
-                                        <Col lg={{ span: 8 }} className='my-1'>
-                                            {elm.message}
-                                        </Col>
-                                        {
-                                            (user?._id === elm.owner._id || user?.role === 'ADMIN') &&
-                                            <Col className="my-1">
-                                                <Row>
-                                                    <Col xs={{ span: 2 }} lg={{ span: 6 }}>
-                                                        <Button onClick={() => editCommentInput(elm._id, elm.message)} className='CommentButtons d-flex justify-content-center' variant="warning">
-                                                            <div>
-                                                                <img src="https://res.cloudinary.com/dkfzj9tmk/image/upload/v1678873300/editwhite_ctrekv.png"
-                                                                    className="mx-auto" />
-                                                            </div>
-                                                        </Button>
+                    comments?.length > 0 &&
+                    <>
+                        <h5>Comments</h5>
+                        <hr className={`${themeValue} hr`} />
+                        {
+                            comments.map((elm, idx) => {
+                                return (
+                                    <Card className={`${themeValue} mt-3`} key={idx}>
+                                        <div className="p-3">
+                                            <Row className="align-items-center">
+                                                <Col className="me-4 my-1" lg={{ span: 1 }}>
+                                                    <OverlayTrigger
+                                                        placement="bottom"
+                                                        overlay={<Tooltip id={`tooltip-bottom`}>
+                                                            {elm.owner.username}
+                                                        </Tooltip>}
+                                                    >
+                                                        <img className={`${themeValue} ownerAvatar`} src={elm.owner.avatar} alt="" />
+                                                    </OverlayTrigger>
+                                                </Col>
+                                                <Col lg={{ span: 8 }} className='my-1'>
+                                                    {elm.message}
+                                                </Col>
+                                                {
+                                                    (user?._id === elm.owner._id || user?.role === 'ADMIN') &&
+                                                    <Col className="my-1">
+                                                        <Row>
+                                                            <Col xs={{ span: 2 }} lg={{ span: 6 }}>
+                                                                <Button onClick={() => editCommentInput(elm._id, elm.message)} className='CommentButtons d-flex justify-content-center' variant="warning">
+                                                                    <div>
+                                                                        <img src="https://res.cloudinary.com/dkfzj9tmk/image/upload/v1678873300/editwhite_ctrekv.png"
+                                                                            className="mx-auto" />
+                                                                    </div>
+                                                                </Button>
+                                                            </Col>
+                                                            <Col xs={{ span: 2 }} lg={{ span: 6 }}>
+                                                                <Button onClick={() => deleteComment(elm._id)} className='CommentButtons d-flex justify-content-center' variant="danger">
+                                                                    <div>
+                                                                        <img src="https://res.cloudinary.com/dkfzj9tmk/image/upload/v1678874366/xwhite2_l8yqvt.png"
+                                                                            className="mx-auto" />
+                                                                    </div>
+                                                                </Button>
+                                                            </Col>
+                                                        </Row>
                                                     </Col>
-                                                    <Col xs={{ span: 2 }} lg={{ span: 6 }}>
-                                                        <Button onClick={() => deleteComment(elm._id)} className='CommentButtons d-flex justify-content-center' variant="danger">
-                                                            <div>
-                                                                <img src="https://res.cloudinary.com/dkfzj9tmk/image/upload/v1678874366/xwhite2_l8yqvt.png"
-                                                                    className="mx-auto" />
-                                                            </div>
-                                                        </Button>
-                                                    </Col>
-                                                </Row>
-                                            </Col>
-                                        }
-                                    </Row>
+                                                }
+                                            </Row>
 
-                                </div>
-                            </Card>
-                        )
-                    })
+                                        </div>
+                                    </Card>
+                                )
+                            })
+
+                        }
+                    </>
             }
         </>
     )
